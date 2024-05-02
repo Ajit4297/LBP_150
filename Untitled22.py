5aeb25dcd84109f8484da273df88893fa4a7be62
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-05-02 12:31:21 +0530

    Add files via upload

---
+++ Untitled22.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+l = '10 20 30 40'.split()
+l1="".join(l)
+s=''
+for i in range (len(l1)-1,-1,-1):
+      s=s+l1[i]
+print(s,end=" ")
+
+
+# In[ ]:
+
+
+
+
